3f09ea347b7e763cf7ec120087b8b9d4087f81bd
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-08-10 00:42:34 +0530

    feat: some progress on baby function
    functions
    if else expression
    definition/name

--- src/baby.hs
+++ src/baby.hs
@@ -5,3 +5,16 @@ doubleUs x y = x * 2 + y * 2
 -- combining simple functions to make complex functions
 doubleUs_ :: Num a => a -> a -> a
 doubleUs_ x y = doubleMe x + doubleMe y
+
+doubleSmallNumber :: (Ord a, Num a) => a -> a
+doubleSmallNumber x = 
+  -- if else is an expression
+  if x > 100
+    then x
+    else doubleMe x
+
+doubleSmallNumber' :: (Num a, Ord a) => a -> a
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1  
+
+-- definition (name) - function that doesn't take any parameters
+conanO'Brien = "It's me, ConanO'Brien!"
