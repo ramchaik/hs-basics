cb12381f002b857b9ead1296d098e3f9d5f13676
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-08-11 00:39:08 +0530

    feat: intro to list

---
+++ src/list.hs
@@ -0,0 +1,32 @@
+lostNumbers = [1,2,3,4,5]
+
+-- list are strings
+-- "abc"
+
+-- concat operation
+-- [1,2,3] ++ [2,3]
+-- "abc" ++ "qwe"
+-- ['w', 'o'] ++ ['o', 't']
+
+-- cons operation, also for concat operation
+-- 1:[1,2]
+-- 'a':"qwe"
+
+-- value at index in a list
+-- [1,2,3] !! 1 
+-- "Steve Buscemi" !! 6  
+
+-- List functions
+-- head [5,4,3,2,1]
+-- tail [5,4,3,2,1]
+-- last [5,4,3,2,1]
+-- init [5,4,3,2,1] 
+-- length [5,4,3,2,1]
+-- null [1,2,3] 
+-- reverse [5,4,3,2,1]
+-- take 3 [5,4,3,2,1]
+-- drop 3 [8,4,2,1,5,6]  
+-- minimum [8,4,2,1,5,6]
+-- maximum [1,9,2,3,4] 
+-- sum [5,2,1,6,3,2,5,7]
+-- product [6,2,1,2] 
